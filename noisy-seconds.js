5c8b03e4d2d1dcf8409e4b8ba6775dd1be030056
Tim Born <timborn@deltacfax.com>
2017-05-21 16:51:30 -0500

    starting to merge timer with metronome to make a noise every second

---
+++ noisy-seconds.js
@@ -0,0 +1,25 @@
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+
+    function updateTime() {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    };
+
+    setInterval( updateTime, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
